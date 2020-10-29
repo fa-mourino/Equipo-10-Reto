f5dc1926cb38495c36dd7289f5b4f9d95ff360c7
Miguel Flores Alvarez <Miguel@Miguels-MacBook-Pro.local>
2020-10-29 11:46:56 -0600

    modifi

--- Paint/Paint.py
+++ Paint/Paint.py
@@ -1,4 +1,3 @@
-#Program Shapes
 from turtle import *
 from freegames import vector
 
@@ -24,15 +23,42 @@ def square(start, end):
 
 def circle(start, end):
     "Draw circle from start to end."
-    pass  # TODO
+    up()
+    goto(start.x, start.y)
+    down()
+    begin_fill()
+
+    for count in range(3):
+        forward(end.x - start.x)
+        left(120)
+
+    end_fill()
 
 def rectangle(start, end):
     "Draw rectangle from start to end."
-    pass  # TODO
+    up()
+    goto(start.x, start.y)
+    down()
+    begin_fill()
+
+    for count in range(4):
+        forward(end.x - start.x)
+        left(90)
+        
+    end_fill()
 
 def triangle(start, end):
     "Draw triangle from start to end."
-    pass  # TODO
+    up()
+    goto(start.x, start.y)
+    down()
+    begin_fill()
+
+    for count in range(3):
+        forward(end.x - start.x)
+        left(120)
+
+    end_fill()
 
 def tap(x, y):
     "Store starting point or draw shape."
