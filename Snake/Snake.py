8fa9f3274e16444fbddbb382034d864d31b893e1
Miguel Flores Alvarez <Miguel@Miguels-MacBook-Pro.local>
2020-10-29 14:05:33 -0600

    CV

--- Snake/Snake.py
+++ Snake/Snake.py
@@ -3,8 +3,8 @@ from random import randrange
 from freegames import square, vector
 
 food = vector(0, 0)
-snake = [vector(100, 0)]
-aim = vector(0, -100)
+snake = [vector(5, 0)]
+aim = vector(0, -5)
 
 def change(x, y):
     "Change snake direction."
@@ -13,15 +13,15 @@ def change(x, y):
 
 def inside(head):
     "Return True if head inside boundaries."
-    return -2000 < head.x < 1900 and -2000 < head.y < 1900
+    return -100 < head.x < 90 and -100 < head.y < 90
 
 def move():
     "Move snake forward one segment."
-    head = snake[-10].copy()
+    head = snake[-1].copy()
     head.move(aim)
 
     if not inside(head) or head in snake:
-        square(head.x, head.y, 90, 'red')
+        square(head.x, head.y, 4, 'red')
         update()
         return
 
@@ -29,27 +29,27 @@ def move():
 
     if head == food:
         print('Snake:', len(snake))
-        food.x = randrange(-150, 150) * 100
-        food.y = randrange(-150, 150) * 100
+        food.x = randrange(-7.5, 7.5) * 10
+        food.y = randrange(-7.5, 7.5) * 10
     else:
         snake.pop(0)
 
     clear()
 
     for body in snake:
-        square(body.x, body.y, 90, 'black')
+        square(body.x, body.y, 4, 'black')
 
-    square(food.x, food.y, 90, 'green')
+    square(food.x, food.y, 4, 'green')
     update()
-    ontimer(move, 1000)
+    ontimer(move, 100)
 
-setup(4200, 4200, 3700, 0)
+setup(420, 420, 370, 0)
 hideturtle()
 tracer(False)
 listen()
-onkey(lambda: change(100, 0), 'Right')
-onkey(lambda: change(-100, 0), 'Left')
-onkey(lambda: change(0, 100), 'Up')
-onkey(lambda: change(0, -100), 'Down')
+onkey(lambda: change(5, 0), 'Right')
+onkey(lambda: change(-5, 0), 'Left')
+onkey(lambda: change(0, 5), 'Up')
+onkey(lambda: change(0, -5), 'Down')
 move()
 done()
